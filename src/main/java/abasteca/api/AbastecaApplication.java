d069ea9bc29fa7499df947227f17d57bbc520145
Matheus <m.cavalcantir@outlook.com>
2023-08-23 19:50:20 -0300

    first commit

---
+++ src/main/java/abasteca/api/AbastecaApplication.java
@@ -0,0 +1,13 @@
+package abasteca.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbastecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbastecaApplication.class, args);
+	}
+
+}
