bacf3d8377dffacdd4dd55727e7ec3138677ec85
Matheus <m.cavalcantir@outlook.com>
2023-08-23 20:34:49 -0300

    feat: criacao inicial da tabela de abastecimento

---
+++ src/main/resources/db/migration/V6_1_0__create-table-abastecimento.sql
@@ -0,0 +1,6 @@
+create table Abastecimento (
+	codigoAbastecimento int (10) AUTO_INCREMENT not null,
+	combustivel varchar (5) not null,
+	quantidade float (5,5) not null,
+	primary key (codigoAbastecimento)
+);
