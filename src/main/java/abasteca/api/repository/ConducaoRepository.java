59037ace41057ec5fcc9e8f7f4d492d97f0a2256
Gandalf The Grey <58261178+matheusrebola@users.noreply.github.com>
2023-08-24 21:39:36 +0000

    feat: configuração inicial das repositorys

---
+++ src/main/java/abasteca/api/repository/ConducaoRepository.java
@@ -0,0 +1,9 @@
+package abasteca.api.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConducaoRepository extends JpaRepository<Conducao, Long> {}
