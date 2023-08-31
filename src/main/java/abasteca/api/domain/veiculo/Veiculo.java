c733b211c6b821bf249515bc9a49fd9ff250ca9f
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 13:29:48 -0300

    feat: configuração da classe veiculo, com seus atributos, método de exclusão lógica, atualização e construtor

--- src/main/java/abasteca/api/domain/veiculo/Veiculo.java
+++ src/main/java/abasteca/api/domain/veiculo/Veiculo.java
@@ -1,4 +1,62 @@
 package abasteca.api.domain.veiculo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Table(name = "veiculos")
+@Entity(name = "Veiculo")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Veiculo {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String marca;
+    private String modelo;
+    private String versao;
+    private String ano;
+    private String motor;
+    private BigDecimal peso;
+    private Boolean ativo;
+
+    public Veiculo(DadosCadastroVeiculo dados){
+        this.ativo = true;
+        this.marca = dados.marca();
+        this.modelo = dados.modelo();
+        this.versao = dados.versao();
+        this.ano = dados.ano();
+        this.motor = dados.motor();
+        this.peso = dados.peso();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoVeiculo dados){
+        if (dados.marca() != null){
+            this.marca = dados.marca();
+        }
+        if (dados.modelo() != null){
+            this.modelo = dados.modelo();
+        }
+        if (dados.versao() != null){
+            this.versao = dados.versao();
+        }
+        if (dados.ano() != null){
+            this.ano = dados.ano();
+        }
+        if (dados.motor() != null){
+            this.motor = dados.motor();
+        }
+        if (dados.peso() != null){
+            this.peso = dados.peso();
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
