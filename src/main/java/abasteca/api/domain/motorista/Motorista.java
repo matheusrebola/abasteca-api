17cb2cf51f94f17eff2572d9c5203f399c2de532
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 13:31:03 -0300

    feat: configuração da classe motorista, com seus atributos, método de exclusão lógica, atualização e construtor

--- src/main/java/abasteca/api/domain/motorista/Motorista.java
+++ src/main/java/abasteca/api/domain/motorista/Motorista.java
@@ -1,4 +1,41 @@
 package abasteca.api.domain.motorista;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Table(name = "motoristas")
+@Entity(name = "Motorista")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Motorista {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime dataNascimento;
+    private Sexo sexo;
+    private Boolean ativo;
+
+    public Motorista(DadosCadastroMotorista dados){
+        this.ativo = true;
+        this.dataNascimento = dados.dataNascimento();
+        this.sexo = dados.sexo();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoMotorista dados){
+        if(dados.dataNascimento() != null){
+            this.dataNascimento = dados.dataNascimento();
+        }
+        if (dados.sexo() != null){
+            this.sexo = dados.sexo();
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
