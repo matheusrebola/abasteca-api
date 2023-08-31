113dfd59bb5f2255e02d49c94b9d9ba7d43cc618
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 12:24:55 -0300

    feat: criação do enum dos avatares utilizando os arquétipos de jung

---
+++ src/main/java/abasteca/api/domain/conducao/Avatar.java
@@ -0,0 +1,16 @@
+package abasteca.api.domain.conducao;
+
+public enum Avatar {
+    INOCENTE,
+    SABIO,
+    HEROI,
+    FORADALEI,
+    EXPLORADOR,
+    MAGO,
+    PESSOACOMUM,
+    AMANTE,
+    BOBO,
+    CUIDADOR,
+    CRIADOR,
+    GOVERNANTE
+}
