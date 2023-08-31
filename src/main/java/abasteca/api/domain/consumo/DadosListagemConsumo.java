1287feb3e465cd1e11b49f27c1ccca69ddeedd03
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 16:22:11 -0300

    feat: adição do construtor ao DTO

--- src/main/java/abasteca/api/domain/consumo/DadosListagemConsumo.java
+++ src/main/java/abasteca/api/domain/consumo/DadosListagemConsumo.java
@@ -2,4 +2,8 @@ package abasteca.api.domain.consumo;
 
 import java.math.BigDecimal;
 
-public record DadosListagemConsumo(Long id, BigDecimal kmPrevista){}
+public record DadosListagemConsumo(Long id, BigDecimal kmPrevista){
+    public DadosListagemConsumo(Consumo consumo){
+        this(consumo.getId(), consumo.getKmPrevista());
+    }
+}
