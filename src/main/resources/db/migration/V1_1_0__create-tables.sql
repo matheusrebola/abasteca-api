49e6106c0a0b6cc904c5592bfbfb23521a3e554e
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 12:27:10 -0300

    refactor: alteração no nome das tabelas; foram colocadas no plural

--- src/main/resources/db/migration/V1_1_0__create-tables.sql
+++ src/main/resources/db/migration/V1_1_0__create-tables.sql
@@ -1,31 +1,31 @@
-create table Consumo (
+create table Consumos (
 	codigoConsumo int(10) AUTO_INCREMENT not null,
 	kmPrevista double (20, 20) not null,
 	primary key (codigoConsumo)
 );
 
-create table Motorista(
+create table Motoristas (
 	codigoMotorista int (10) AUTO_INCREMENT not null,
 	idade int (2),
 	sexo varchar (1),
 	primary key (codigoMotorista)
 );
 
-create table Conducao (
+create table Conducoes (
 	codigoConducao int (10) AUTO_INCREMENT not null,
 	avatarCondutor varchar (20),
 	estiloDirecao varchar (50),
 	primary key (codigoConducao)
 );
 
-create table Trajeto (
+create table Trajetos (
 	codigoTrajeto int (10) AUTO_INCREMENT not null,
 	longitude double (10,10),
 	latitude double (10,10),
 	primary key (codigoTrajeto)
 ); 
 
-create table Veiculo (
+create table Veiculos (
 	codigoVeiculo int (10) AUTO_INCREMENT not null,
 	marca varchar (20) not null,
 	modelo varchar (20) not null,
@@ -37,7 +37,7 @@ create table Veiculo (
 	primary key (codigoVeiculo)
 ); 
 
-create table Abastecimento (
+create table Abastecimentos (
 	codigoAbastecimento int (10) AUTO_INCREMENT not null,
 	combustivel varchar (5) not null,
 	quantidade float (5,5) not null,
