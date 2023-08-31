754a70c5609ef2eb9cdd781e90c7ca3ef9b410d1
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 13:32:07 -0300

    feat: configuração da classe trajeto, com seus atributos, método de exclusão lógica, atualização e construtor

--- src/main/java/abasteca/api/domain/trajeto/Trajeto.java
+++ src/main/java/abasteca/api/domain/trajeto/Trajeto.java
@@ -1,4 +1,35 @@
 package abasteca.api.domain.trajeto;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Table(name = "trajetos")
+@Entity(name = "Trajeto")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Trajeto {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal longitude;
+    private BigDecimal latitude;
+    private Boolean ativo;
+
+    public Trajeto(DadosCadastroTrajeto dados){
+        this.ativo = true;
+        this.latitude = dados.latitude();
+        this.longitude = dados.longitude();
+    }
+    public void atualizarInformacoes(DadosAtualizacaoTrajeto dados){
+
+    }
+    public void excluir() {
+        this.ativo = false;
+    }
 }
