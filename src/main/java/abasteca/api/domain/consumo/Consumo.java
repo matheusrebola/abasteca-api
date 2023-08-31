0979cb7efef374eb99901d9fb69f7d9df46d49fc
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 13:28:17 -0300

    feat: configuração da classe consumo, com seus atributos, método de exclusão lógica, atualização e construtor

--- src/main/java/abasteca/api/domain/consumo/Consumo.java
+++ src/main/java/abasteca/api/domain/consumo/Consumo.java
@@ -1,4 +1,36 @@
 package abasteca.api.domain.consumo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Consumo {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal kmPrevista;
+    private Boolean ativo;
+
+    public Consumo(DadosCadastroConsumo dados){
+        this.kmPrevista = dados.kmPrevista();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoConsumo dados){
+        if (dados.kmPrevista() != null){
+            this.kmPrevista = dados.kmPrevista();
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
