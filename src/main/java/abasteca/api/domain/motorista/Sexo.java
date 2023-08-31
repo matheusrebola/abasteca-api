4ac214ff99d4b674ba992545438ce03369886846
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 13:26:25 -0300

    feat: criado o enum com alguns tipos de gêneros

---
+++ src/main/java/abasteca/api/domain/motorista/Sexo.java
@@ -0,0 +1,15 @@
+package abasteca.api.domain.motorista;
+
+public enum Sexo {
+    MASCULINO,
+    FEMININO,
+    TRANSGENERO,
+    GENERONEUTRO,
+    NAOBINARIO,
+    AGENERO,
+    PANGENERO,
+    GENDERQUEER,
+    TWOSPIRIT,
+    TERCEIROGENERO,
+    OUTRO
+}
