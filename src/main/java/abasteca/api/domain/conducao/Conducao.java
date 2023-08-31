51b5ec6cb813956b47213b45ccffe1832d689cdd
matheusrebola <m.cavalcantir@outlook.com>
2023-08-31 12:30:08 -0300

    feat: criação da classe condução, com seus atributos, métodos de atualização e exclusão lógica e construtor

--- src/main/java/abasteca/api/domain/conducao/Conducao.java
+++ src/main/java/abasteca/api/domain/conducao/Conducao.java
@@ -1,4 +1,43 @@
 package abasteca.api.domain.conducao;
 
+import abasteca.api.domain.abastecimento.DadosAtualizacaoAbastecimento;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "conducoes")
+@Entity(name = "Conducao")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Conducao {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Avatar avatar;
+    private EstiloDirecao estiloDirecao;
+
+    private Boolean ativo;
+
+    public Conducao(DadosCadastroConducao dados) {
+        this.ativo = true;
+        this.avatar = dados.avatar();
+        this.estiloDirecao = dados.estiloDirecao();
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoConducao dados){
+        if(dados.avatar() != null){
+            this.avatar = dados.avatar();
+        }
+        if(dados.estiloDirecao() != null){
+            this.estiloDirecao = dados.estiloDirecao();
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
